d7486d06832154af0934aea5578f51a5371ff70e
edus44 <eduhhg@gmail.com>
2018-10-27 21:29:44 +0200

    added boardgame

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
 const vm = new Vue({
   el: '#app',
+  store,
   render: h => h(App),
 })
 
---
+++ src/store/gameModule.js
@@ -0,0 +1,57 @@
+import { Client } from 'boardgame.io/client'
+import game from '@/../game/index'
+
+const config = {
+  numPlayers: 2,
+  /* Multiplayer options */
+  // playerID: 'def',
+  // gameID: 'def',
+  // multiplayer: process.env.NODE_ENV === 'production' ? true : { server: 'localhost:8000' },
+}
+
+const client = Client({ game, ...config })
+
+client.connect()
+
+if (process.env.NODE_ENV === 'development') {
+  require('vue-cli-plugin-boardgame/clientDebug').start(client)
+  window.client = client
+}
+
+const state = {
+  G: null,
+  ctx: null,
+  config,
+}
+
+const actions = {
+  init({ commit, state }) {
+    const sync = () => {
+      const { G, ctx } = client.getState()
+      commit('SET_STATE', { G, ctx })
+    }
+
+    client.subscribe(sync)
+    sync()
+  },
+  move(state, { name, args = [] }) {
+    return client.moves[name](...args)
+  },
+}
+
+const mutations = {
+  SET_STATE(state, { G, ctx }) {
+    state.G = Object.freeze(G)
+    state.ctx = Object.freeze(ctx)
+  },
+}
+
+const getters = {}
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+  getters,
+}
